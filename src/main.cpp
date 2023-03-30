9e8871833389fc7159d6b11c4832584bf6f58f21
Zac Joffe <zacharyjoffe@gmail.com>
2023-03-29 22:48:35 -0400

    Change window name to be more friendly to WM

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "Window.h"
 
 int main(int argc, char **argv) {
-    CS488Window::launch(argc, argv, new Window(), 1024, 768, "CS488 Project");
+    CS488Window::launch(argc, argv, new Window(), 1024, 768, "CS488_Project_FPS");
     return 0;
 }
