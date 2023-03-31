23923b8f2785206f4b347f636b321eb0cc2c07b0
Zac Joffe <zacharyjoffe@gmail.com>
2023-03-31 18:51:06 -0400

    Collision test algorithm

--- src/BoundingBox.cpp
+++ src/BoundingBox.cpp
@@ -4,9 +4,9 @@
 
 BoundingBox::BoundingBox(glm::vec2 min, glm::vec2 max) : min(min), max(max) {}
 
-bool BoundingBox::colliding(const BoundingBox & other) const {
-    // TODO
-    return false;
+bool BoundingBox::collisionTest(const BoundingBox & other) const {
+    return max.x >= other.min.x && other.max.x >= min.x
+        && max.y >= other.min.y && other.max.y >= min.y;
 }
 
 std::string BoundingBox::toString() const {
--- src/BoundingBox.h
+++ src/BoundingBox.h
@@ -8,6 +8,6 @@ struct BoundingBox {
     glm::vec2 max;
     BoundingBox(glm::vec2 min, glm::vec2 max);
 
-    bool colliding(const BoundingBox & other) const;
+    bool collisionTest(const BoundingBox & other) const;
     std::string toString() const;
 };
