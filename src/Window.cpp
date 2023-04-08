65084a17c465a89e8c95ea256be76f5ec3ffe0b9
Zac Joffe <zacharyjoffe@gmail.com>
2023-04-08 19:36:07 -0400

    Port CS488Window class

--- src/Window.cpp
+++ src/Window.cpp
@@ -1,18 +1,163 @@
 #include "Window.h"
 
+#include <gl3w/GL/gl3w.h>
 #include "GameState.h"
 #include "MenuState.h"
 #include "State.h"
 
+#include <imgui.h>
+#include <imgui/backends/imgui_impl_glfw.h>
+#include <imgui/backends/imgui_impl_opengl3.h>
+
 #include <iostream>
 #include <memory>
+#include <pmmintrin.h>
+#include <stdexcept>
+
+Window & Window::getInstance() {
+    static Window window;
+    return window;
+}
 
 Window::Window() :
     m_delta_time(0.0f),
     m_last_frame_time(0.0f),
     m_curr_state_value(StateValue::Menu) {}
 
-Window::~Window() {}
+void Window::launch(int width, int height, const std::string & title, float fps) {
+    run(width, height, title, fps);
+}
+
+void Window::run(int width, int height, const std::string & window_title, float target_fps) {
+    m_window_width = width;
+    m_window_height = height;
+    m_window_title = window_title;
+
+    glfwSetErrorCallback([](int error, const char * description){
+        std::cout << "GLFW Error Code: " << error << std::endl
+                  << "GLFW Eror Description: " << description << std::endl;
+    });
+
+    if (!glfwInit()) {
+        throw std::runtime_error("Failed to init glfw");
+    }
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_VISIBLE, GL_TRUE);
+    glfwWindowHint(GLFW_SAMPLES, 0);
+    glfwWindowHint(GLFW_RED_BITS, 8);
+    glfwWindowHint(GLFW_GREEN_BITS, 8);
+    glfwWindowHint(GLFW_BLUE_BITS, 8);
+    glfwWindowHint(GLFW_ALPHA_BITS, 8);
+
+    // m_monitor = glfwGetPrimaryMonitor();
+    // if (!m_monitor) {
+    //     glfwTerminate();
+    //     throw std::runtime_error("Failed to get primary monitor");
+    // }
+
+    m_window = glfwCreateWindow(m_window_width, m_window_height, m_window_title.c_str(), nullptr, nullptr);
+    if (!m_window) {
+        glfwTerminate();
+        throw std::runtime_error("Failed to create glfw window");
+    }
+
+    glfwGetFramebufferSize(m_window, &m_framebuffer_width, &m_framebuffer_height);
+    glfwMakeContextCurrent(m_window);
+
+    // TODO update gl3w
+    gl3wInit();
+    if (!gl3wIsSupported(3, 3)) {
+        throw std::runtime_error("OpenGL not supported");
+    }
+
+    const auto gl_version = glGetString(GL_VERSION);
+    const auto glsl_version = glGetString(GL_SHADING_LANGUAGE_VERSION);
+    std::cout << "OpenGL " << gl_version << ", GLSL " << glsl_version << std::endl;
+
+    register_glfw_callbacks();
+
+    // TODO refactor into method
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(m_window, true);
+    // ImGui_ImplOpenGL3_Init(glsl_version); // FIXME
+    ImGui_ImplOpenGL3_Init("#version 130");
+
+    windowResizeEvent(width, height);
+
+    while (glGetError()) {}
+
+    try {
+        glfwSwapInterval(1);
+
+        // TODO
+        init();
+
+        while (!glfwWindowShouldClose(m_window)) {
+            glfwPollEvents();
+
+            // Start the Dear ImGui frame
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+
+            appLogic();
+            guiLogic();
+
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            draw();
+
+            glfwGetFramebufferSize(m_window, &m_framebuffer_width, &m_framebuffer_height);
+            glViewport(0, 0, m_framebuffer_width, m_framebuffer_height);
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+            glfwSwapBuffers(m_window);
+        }
+    } catch (const std::exception & e) {
+        std::cerr << "Exception thrown: " << e.what() << std::endl;
+    } catch (...) {
+        std::cerr << "Uncaught exception thrown! Terminating..." << std::endl;
+    }
+
+    glfwDestroyWindow(m_window);
+}
+
+
+void Window::register_glfw_callbacks() {
+    glfwSetKeyCallback(m_window, [](GLFWwindow * window, int key, int scancode, int action, int mods) {
+        getInstance().keyInputEvent(key, action, mods);
+    });
+    glfwSetWindowSizeCallback(m_window, [](GLFWwindow * window, int width, int height) {
+        getInstance().windowResizeEvent(width, height);
+    });
+    // glfwSetScrollCallback(m_window, [](GLFWwindow * window, double xoffset, double yoffset) {
+    //     getInstance().mouseScrollEvent(xoffset, yoffset);
+    // });
+    glfwSetMouseButtonCallback(m_window, [](GLFWwindow * window, int button, int action, int mods) {
+        getInstance().mouseButtonInputEvent(button, action, mods);
+    });
+    glfwSetCursorPosCallback(m_window, [](GLFWwindow * window, double xpos, double ypos) {
+        getInstance().cursorPositionEvent(xpos, ypos);
+    });
+    // glfwSetCursorEnterCallback(m_window, [](GLFWwindow * window, int entered) {
+    //     getInstance().cursorEnterWindowEvent(entered);
+    // })
+}
 
 void Window::init() {
     ImGuiIO & io = ImGui::GetIO();
@@ -20,7 +165,7 @@ void Window::init() {
     m_font_normal = io.Fonts->AddFontFromFileTTF("./assets/fonts/roboto/Roboto-Regular.ttf", 16);
 
     // always start in menu state
-    m_curr_state = std::make_unique<MenuState>(m_framebufferWidth, m_framebufferHeight, m_font_title, m_font_normal);
+    m_curr_state = std::make_unique<MenuState>(m_framebuffer_width, m_framebuffer_height, m_font_title, m_font_normal);
     m_game_context = std::make_unique<GameContext>();
     glfwSetInputMode(m_window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
 }
@@ -49,39 +194,25 @@ void Window::draw() {
     m_curr_state->draw();
 }
 
-void Window::cleanup() {}
+// void Window::cleanup() {}
 
 
 // EVENT HANDLERS
-bool Window::cursorEnterWindowEvent(int entered) {
-    return false;
+void Window::cursorPositionEvent(double xpos, double ypos) {
+    m_curr_state->handleMouseMove(xpos, ypos);
 }
 
-bool Window::mouseMoveEvent(double xPos, double yPos) {
-    m_curr_state->handleMouseMove(xPos, yPos);
-
-    // this value is not used by the caller in the cs488 framework
-    return false;
+void Window::mouseButtonInputEvent(int button, int action, int mods) {
+    m_curr_state->handleMouseButtonInput(button, action, mods);
 }
 
-bool Window::mouseButtonInputEvent(int button, int actions, int mods) {
-    m_curr_state->handleMouseButtonInput(button, actions, mods);
-
-    return false;
+void Window::windowResizeEvent(int width, int height) {
+    m_window_width = width;
+    m_window_height = height;
 }
 
-bool Window::mouseScrollEvent(double xOffSet, double yOffSet) {
-    return false;
-}
-
-bool Window::windowResizeEvent(int width, int height) {
-    return false;
-}
-
-bool Window::keyInputEvent(int key, int action, int mods) {
+void Window::keyInputEvent(int key, int action, int mods) {
     m_curr_state->handleKeyInput(key, action, mods);
-
-    return true;
 }
 
 void Window::toggleState() {
@@ -90,7 +221,7 @@ void Window::toggleState() {
             std::cout << "switching to game state" << std::endl;
 
             m_curr_state_value = StateValue::Game;
-            m_curr_state = std::make_unique<GameState>(*m_game_context, m_framebufferWidth, m_framebufferHeight);
+            m_curr_state = std::make_unique<GameState>(*m_game_context, m_framebuffer_width, m_framebuffer_height);
             glfwSetInputMode(m_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
             break;
         }
@@ -98,7 +229,7 @@ void Window::toggleState() {
             std::cout << "switching to game state" << std::endl;
 
             m_curr_state_value = StateValue::Menu;
-            m_curr_state = std::make_unique<MenuState>(m_framebufferWidth, m_framebufferHeight, m_font_title, m_font_normal);
+            m_curr_state = std::make_unique<MenuState>(m_framebuffer_width, m_framebuffer_height, m_font_title, m_font_normal);
             glfwSetInputMode(m_window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
             break;
         }
--- src/Window.h
+++ src/Window.h
@@ -1,36 +1,54 @@
 #pragma once
 
-#include "cs488-framework/CS488Window.hpp"
+// #include "cs488-framework/CS488Window.hpp"
+#define GLFW_INCLUDE_GLCOREARB
+#include <GLFW/glfw3.h>
+
 #include "GameContext.h"
 #include "GameState.h"
 #include "MenuState.h"
 #include "State.h"
-#include "imgui.h"
+#include <imgui.h>
 
 #include <memory>
+#include <string>
 
-class Window : public CS488Window {
+class Window {
 public:
-    Window();
-    virtual ~Window();
+    static Window & getInstance();
+    Window(Window const &) = delete;
+    void operator=(Window const &) = delete;
 
-protected:
-    void init() override;
-    void appLogic() override;
-    void guiLogic() override;
-    void draw() override;
-    void cleanup() override;
-
-    bool cursorEnterWindowEvent(int entered) override;
-    bool mouseMoveEvent(double xPos, double yPos) override;
-    bool mouseButtonInputEvent(int button, int actions, int mods) override;
-    bool mouseScrollEvent(double xOffSet, double yOffSet) override;
-    bool windowResizeEvent(int width, int height) override;
-    bool keyInputEvent(int key, int action, int mods) override;
+    void launch(int width, int height, const std::string & title, float fps = 60.0f);
 
 private:
+    Window();
+
+    void run(int width, int height, const std::string & window_title, float target_fps);
+    void register_glfw_callbacks();
+
+    void init();
+    void appLogic();
+    void guiLogic();
+    void draw();
+
+    // void cursorEnterWindowEvent(int entered);
+    void cursorPositionEvent(double xpos, double ypos);
+    void mouseButtonInputEvent(int button, int action, int mods);
+    // void mouseScrollEvent(double xoffset, double yoffset);
+    void windowResizeEvent(int width, int height);
+    void keyInputEvent(int key, int action, int mods);
+
     void toggleState();
     void calculateDeltaTime();
+
+    // GLFWmonitor * m_monitor;
+    GLFWwindow * m_window;
+    std::string m_window_title;
+    int m_window_width;
+    int m_window_height;
+    int m_framebuffer_width;
+    int m_framebuffer_height;
 
     StateValue m_curr_state_value;
     std::unique_ptr<State> m_curr_state;
